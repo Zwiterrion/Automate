1222f4d7e6776ab50272579f0470a0e11a8e95b3
Etienne Anne <etianne@info-velenx.iut.bx1>
2014-04-04 10:44:43 +0200

    premier commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int tab[] =
+
+void createAutomate(string intitule, int nbEtats, int nbTransitions, int nbEtats_terminaux, )
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
